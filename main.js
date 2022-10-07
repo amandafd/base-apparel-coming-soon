722b8e4dca885ece369d477a4e0da9efe0918e07
amandafd <amanda_dionisio@hotmail.com>
2022-10-07 19:21:00 -0300

    melhorando código

---
+++ main.js
@@ -0,0 +1,5 @@
+var campoEmail = document.getElementById("btn-enviar");
+function exibeConfirmacao() {
+    if(campoEmail == valid)
+    alert('Cadastro confirmado. Confira sua caixa de email!');
+}
